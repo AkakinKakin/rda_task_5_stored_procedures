c3943b655b89919e78aa0f4794dd5d2f71b89553
Eldar <eldardemchenko19@mail.com>
2025-06-29 00:16:22 +0300

    fixed0.3

--- task.sql
+++ task.sql
@@ -5,7 +5,8 @@ DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(IN warehouseID INT)
 BEGIN
 	SELECT Products.Name AS product_name, ProductInventory.WarehouseAmount AS amount
-    FROM ProductInventory
-	JOIN Products ON ProductInventory.ProductID = Products.ProductID;
+	FROM ProductInventory
+	JOIN Products ON ProductInventory.ProductID = Products.ID
+	WHERE ProductInventory.WarehouseID = @warehouseID;
 END //
 DELIMITER ;
